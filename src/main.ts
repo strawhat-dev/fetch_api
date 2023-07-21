65c54ca732e902ca37126c1e240f69d637ec963e
luffy <kn37.dev@gmail.com>
2023-07-20 21:20:44 -0700

    delete scratch file

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { isPrimitive } from '@/utils';
-
-const cloneFn = (fn: Function) => new Function(`return ${fn}`)();
-const fn = () => 'hello';
-fn.key = 'value';
-
-const fn2 = cloneFn(fn);
-
-console.log(fn2());
